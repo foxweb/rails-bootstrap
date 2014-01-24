bf7c8ffc160fd464569412aeab612192e338913d
Aleksey Kurepin <lesha@kurepin.com>
2014-01-24 10:21:41 +0400

    small fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ RailsBootstrap::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', only_path: true }
 end
